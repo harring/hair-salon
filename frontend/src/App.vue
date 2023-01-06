e8f3f8ea1066795d93024a1dd16255f8fe41b82e
A.Törngren <91566171+Toerngren@users.noreply.github.com>
2023-01-06 17:18:18 +0100

    Add box shadow

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div class="circle"></div>
-  <nav id="nav">
-    <div class="nav-left">
-      <h1>
-        <a href="/"
-          ><img id="img" alt="Hair Salon logo" src="../src/assets/scissor.png"
-        /></a>
-        Hair Salon
-      </h1>
-    </div>
-    <div class="nav-right">
-      <router-link to="/"> Home </router-link> |
-      <router-link to="/services"> Services </router-link> |
-      <router-link to="/about"> About</router-link>
-    </div>
-  </nav>
+  <NavBar />
   <router-view />
 </template>
 
 <script>
 // @ is an alias to /src
+import NavBar from "@/components/NavBar.vue";
 
 export default {
   name: "App",
+  components: {
+    NavBar,
+  },
 };
 </script>
 
@@ -38,6 +28,9 @@ body {
   margin: 0px;
   padding: 0px;
 }
+nav a.router-link-exact-active {
+  color: #db2c44;
+}
 .circle {
   width: 80%;
   height: 80%;
@@ -45,70 +38,6 @@ body {
   border-bottom-right-radius: 100%;
   position: fixed;
   z-index: -1;
-}
-#img {
-  width: 70px;
-  height: 70px;
-  vertical-align: middle;
-  transform: rotate(-0.25turn);
-}
-
-h1 {
-  display: flex;
-  align-items: center;
-  font-size: 32px;
-}
-
-#nav {
-  position: sticky;
-  top: 0;
-  background: whitesmoke;
-  border: 2px solid grey;
-  border-radius: 20px;
-  height: 70px;
-  display: flex;
-  align-items: stretch;
-  width: 95%;
-  margin: 0 auto;
-}
-
-.nav-right {
-  margin-left: auto;
-  font-size: 22px;
-  display: flex;
-  align-items: center;
-  padding-right: 5px;
-}
-
-.nav-left {
-  display: flex;
-  align-items: center;
-  padding-left: 5px;
-}
-
-nav a {
-  font-weight: bold;
-  color: black;
-  text-decoration: none;
-  margin-left: 5px;
-  margin-right: 5px;
-}
-
-nav a.router-link-exact-active {
-  color: #db2c44;
-}
-
-/* apply this style for screens smaller than 500px */
-@media (max-width: 500px) {
-  #img {
-    width: 50px;
-    height: 50px;
-  }
-  h1 {
-    font-size: 20px;
-  }
-  .nav-right {
-    font-size: 18px;
-  }
+  box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.5);
 }
 </style>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,90 @@
+<template>
+  <nav id="nav">
+    <div class="nav-left">
+      <h1>
+        <a href="/"
+          ><img id="img" alt="Hair Salon logo" src="../assets/scissor.png"
+        /></a>
+        Hair Salon
+      </h1>
+    </div>
+    <div class="nav-right">
+      <router-link to="/"> Home </router-link> |
+      <router-link to="/services"> Services </router-link> |
+      <router-link to="/about"> About</router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style scoped>
+#img {
+  width: 70px;
+  height: 70px;
+  vertical-align: middle;
+  transform: rotate(-0.25turn);
+}
+
+h1 {
+  display: flex;
+  align-items: center;
+  font-size: 32px;
+}
+
+#nav {
+  position: sticky;
+  top: 0;
+  background: whitesmoke;
+  border: 2px solid grey;
+  border-bottom-right-radius: 20px;
+  border-bottom-left-radius: 20px;
+  height: 70px;
+  display: flex;
+  align-items: stretch;
+  width: 95%;
+  margin: 0 auto;
+}
+
+.nav-right {
+  margin-left: auto;
+  font-size: 22px;
+  display: flex;
+  align-items: center;
+  padding-right: 5px;
+}
+
+.nav-left {
+  display: flex;
+  align-items: center;
+  padding-left: 5px;
+}
+
+nav a {
+  font-weight: bold;
+  color: black;
+  text-decoration: none;
+  margin-left: 5px;
+  margin-right: 5px;
+}
+
+/* apply this style for screens smaller than 500px */
+@media (max-width: 500px) {
+  #img {
+    width: 50px;
+    height: 50px;
+  }
+  h1 {
+    font-size: 20px;
+  }
+  .nav-right {
+    font-size: 18px;
+  }
+}
+</style>
