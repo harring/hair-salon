c8d73d261fb6b5af5e76aeee1308a3b89dd105cb
A.Törngren <91566171+Toerngren@users.noreply.github.com>
2023-01-07 00:15:14 +0100

    Add a slider with placeholder content

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,77 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <h1>&#x2B05; Some headline &#x27A1;</h1>
+    <div class="HomeView">
+      <div>
+        <AboutComponent />
+      </div>
+      <div>
+        <ServicesComponent />
+      </div>
+    </div>
+  </div>
+  <div class="item">
+    <h3>Scroll right for more</h3>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import AboutComponent from "@/components/AboutComponent.vue";
+import ServicesComponent from "@/components/ServicesComponent.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    AboutComponent,
+    ServicesComponent,
   },
 };
 </script>
+
+<style scoped>
+.container {
+  width: 80%;
+  margin: auto;
+}
+h1 {
+  color: white;
+}
+
+.HomeView {
+  display: flex;
+  flex-wrap: nowrap;
+  overflow-x: auto;
+  scroll-behavior: smooth;
+  width: 100%;
+  height: 100%;
+}
+
+.HomeView > div {
+  flex: 0 0 auto;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+
+.HomeView::-webkit-scrollbar {
+  width: 2px;
+  height: 10px;
+}
+
+.HomeView::-webkit-scrollbar-thumb {
+  background-color: darkred;
+  border-radius: 10px;
+}
+
+.HomeView::-webkit-scrollbar-thumb:hover {
+  background-color: palevioletred;
+}
+
+/* apply this style for screens smaller than 500px */
+@media (max-width: 500px) {
+  h1 {
+    font-size: 24px;
+  }
+}
+</style>
